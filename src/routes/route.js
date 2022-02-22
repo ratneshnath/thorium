ac3551aef070a24c5fd55f0a1a42e75921c77459
ratneshnath <ratnesh99nath@gmail.com>
2022-02-22 19:41:52 +0530

    message

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,12 @@
+let obj = require('./logger')
 const express = require('express');
 
 const router = express.Router();
 
+
 router.get('/test-me', function (req, res) {
+    obj.printMyMessage('thorium')
+    console.log(obj.url)
     res.send('My first ever api!')
 });
 
