db105dbc11df8d203eca80133935c426768263dc
ratneshnath <ratnesh99nath@gmail.com>
2022-03-02 03:29:23 +0530

    excellent

--- src/routes/route.js
+++ src/routes/route.js
@@ -36,6 +36,29 @@ router.get("/dateManipulations", function (req, res) {
     console.log(x)
 
     res.send({ msg: "all good"})
+
+
+
+    
+
+    const express = require('express'); 
+
+const router = express.Router(); 
+
+
+const allController = require('../controllers/allController') 
+
+
+
+router.post('/createNewAuthor', allController.createNewAuthor) 
+router.post('/createNewBook', allController.createNewBook) 
+router.get('/allBooks', allController.allBooks) 
+router.get('/updatedBookPrice', allController.upadatedBookPrice) 
+router.get('/authorsName', allController.authorsName) 
+
+
+
+
 })
 
 module.exports = router;
