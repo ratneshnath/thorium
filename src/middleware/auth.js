fb3d641bf85d92de78f9ee1cb758fea48c1a0e0b
ratneshnath <ratnesh99nath@gmail.com>
2022-03-09 11:53:28 +0530

    done again

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,3 +1,9 @@
-router.get("/users/:userId", userController.getUserData)
- router.put("/users/:userId", userController.updateUser)
- router.delete("/users/:userId", userController.deleteUser)
+const mid1 = function(req,res,next){
+    let xAuthToken = req.headers["x-auth-token"]
+    if(!token)
+    token = req.headers["x-auth-token"]
+    if(!token)
+    return res.send({status: false,msg: "token must be present"});
+    next()
+}
+module.exports.mid1=mid1
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const midware = require("../middleware/auth")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -11,11 +12,11 @@ router.post("/registerUser", userController.registerUser  )
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
+router.get("/users/:userId",midware.mid1, userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
+router.put("/users/:userId",midware.mid1, userController.updateUser)
 
-router.delete("/users/:userId",userController.deleteUser)
+router.delete("/users/:userId",midware.mid1,userController.deleteUser)
 
 
 module.exports = router;
